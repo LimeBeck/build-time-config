21d959f54d955e63b0d6ae059e9097ae23f72b4f
limebeck <mail@limebeck.dev>
2023-08-27 14:41:40 +0300

    Revert jvmToolchain version. Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.mavenPublish)
     alias(libs.plugins.pluginPublish)
     alias(libs.plugins.versions)
     `java-gradle-plugin`
